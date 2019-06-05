922ec79693d7294646063e6421409c8271e72428
brad <mybradtw@gmail.com>
2019-06-05 10:34:55 +0800

    first commit

---
+++ brad01.js
@@ -0,0 +1,7 @@
+// 不需要再加上 script 的 tag
+var var1 = "Hello, World"  // 敘述句結尾分號可以不需要, 但是不建議
+// alert(Var1);    變數大小寫嚴格區分
+alert(var1);        // 函數/指令大小寫沒有嚴格區分
+//document.write(typeof(Var1));   // 沒有初始化的變數, 將會是 undefined
+document.write(typeof(var1) + "<br>");
+
